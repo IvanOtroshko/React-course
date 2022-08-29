38ee1fec4dc0e232a56bc422d5217f1e642b1e78
Ivan <ivan.otroshko228@gmail.com>
2022-08-29 11:21:26 +0300

    Hw-2 нет 3 задания

--- React/my-app/src/App.tsx
+++ React/my-app/src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import { Button } from "./components/Button";
+import { Clicker } from "./components/Clicker";
 import { Input } from "./components/Input";
+import { PostList } from "./components/Posts/List";
 import { Title } from "./components/Title";
 import { UserCard } from "./components/User";
 
@@ -20,12 +22,14 @@ function App() {
 
   return (
     <div className="App">
-      <Title text="Hi React"/>
+      {/* <Title text="Hi React"/>
       <Button text="Login" onClick={onClickLogin} />
       <Button text="Sign up" onClick={onClickSignUp} />
       <Button text="Logout" onClick={onClickLogout} />
       <Input  placeholder='Введите логин'/>
-      <UserCard UserName={"Ivan Otroshko"} />
+      <UserCard UserName={"Ivan Otroshko"} /> */}
+      <Clicker/>
+      <PostList/>
     </div>
    
   );
